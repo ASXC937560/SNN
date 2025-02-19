1011163dc9f3133b25114c23ba375e3977412e4c
ASXC937560 <asxc115100@gmail.com>
2025-02-19 22:41:00 +0100

    Versión final

--- Entrega_20_Febrero/Entrega_20_de_Febrero.py
+++ Entrega_20_Febrero/Entrega_20_de_Febrero.py
@@ -4,14 +4,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+u_list = [1,2,3]
 #Defino X para hacer la trayectoria a traves de elejir la velocidad como input
-def draw_trajectory(u):
+def draw_trajectory(v,u):
 
     #Defino la aceleración de la gravedad en m/s^2   
     g = 9.8
 
     #Defino el tiempo total de vuelo que tendrá
-    t_flight = 2*u/g
+    t_flight = 2*v/g
 
     #A partir del tiempo total de vuelo creo los intervalos para poder hacer la gráfica
     intervals = np.arange(0, t_flight, 0.001)
@@ -19,33 +20,33 @@ def draw_trajectory(u):
     #Creo una lista vacia para las coordenadas X y Y (la X es para que no se solapen y poder distinguir bien las 3 trayectorias)
     x = []
     y = []
-
+    
     #Hago un loop a través del tiempo para calcular las coordenadas (en concreto solo Y)   
     for t in intervals:
-        x.append(0)
-        y.append(u*t-0.5*g*t*t)
-
+        x.append(u)
+        y.append(v*t-0.5*g*t*t)
+        
     #Genero el gráfico y le añado título y coordenadas X y Y con unidades
-    plt.plot(x, y)
-plt.xlabel('Distance (m)')
+    plt.plot(x,y)
+plt.xlabel('Puntos de separación')
 plt.ylabel('Height (m)')
 plt.title('Projectile motion')
-
+plt.xticks(u_list)
 #Genero la lista de velocidades iniciales m/s
-u_list = [20, 40, 60]
+v_list = [20, 40, 60]
 
-for u in u_list:
-    draw_trajectory(u)
+for v,u in zip(v_list,u_list):
+    draw_trajectory(v,u)
 
     #Calculo la altura máxima respecto a cada velocidad inicial
     g = 9.8
-    print("Altura máxima de  velocidad",u)
-    ymax = (u*u/g)-0.5*g*(u/g)*(u/g)
+    print("Altura máxima de  velocidad",v)
+    ymax = (v*v/g)-0.5*g*(v/g)*(v/g)
     print(ymax)
 
     #Calculo el tiempo de vuelo para cada velocidad
-    print("Tiempo de vuelo de cada velocidad",u)
-    t_flight = 2*u/g
+    print("Tiempo de vuelo de cada velocidad",v)
+    t_flight = 2*v/g
     print(t_flight)
 
 #Muestra la gráfica con una leyenda
