8e6549c12aadd538c491bbfc00a92d74479c068a
ASXC937560 <asxc115100@gmail.com>
2025-02-19 23:54:11 +0100

    versión final definitiva

--- Entrega_20_Febrero/Entrega_20_de_Febrero.py
+++ Entrega_20_Febrero/Entrega_20_de_Febrero.py
@@ -1,17 +1,18 @@
 #Ejemplo de caída libre
 
-#Importo la libreria matemática (numpy) y la de hacer gráficas matplotlib
+#Importo la libreria matemática (numpy) y la libreria de graficar (matplotlib)
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Lista que servirá para separar en X las 3 trayectorias a graficar
 u_list = [1,2,3]
-#Defino X para hacer la trayectoria a traves de elejir la velocidad como input
+#Defino una función para hacer la trayectoria, la v es la velocidad y la u es para poder separar las diferentes caídas libres en X
 def draw_trajectory(v,u):
 
     #Defino la aceleración de la gravedad en m/s^2   
     g = 9.8
 
-    #Defino el tiempo total de vuelo que tendrá
+    #Defino el tiempo total de vuelo en s
     t_flight = 2*v/g
 
     #A partir del tiempo total de vuelo creo los intervalos para poder hacer la gráfica
@@ -21,20 +22,21 @@ def draw_trajectory(v,u):
     x = []
     y = []
     
-    #Hago un loop a través del tiempo para calcular las coordenadas (en concreto solo Y)   
+    #Hago un loop a través del tiempo para calcular la coordenada Y, en X solo está el termino que separa trayectorias (u)
     for t in intervals:
         x.append(u)
         y.append(v*t-0.5*g*t*t)
         
-    #Genero el gráfico y le añado título y coordenadas X y Y con unidades
+    #Genero el gráfico, le añado título y coordenadas X y Y (con unidades), por último hago que en la X hago que solo esten marcados los valores de la u_list
     plt.plot(x,y)
 plt.xlabel('Puntos de separación')
 plt.ylabel('Height (m)')
 plt.title('Projectile motion')
 plt.xticks(u_list)
-#Genero la lista de velocidades iniciales m/s
-v_list = [20, 40, 60]
 
+#Lista de velocidades iniciales m/s
+v_list = [20, 40, 60]
+#Itero las las velocidades iniciales y la separación de las distintas caídas libres
 for v,u in zip(v_list,u_list):
     draw_trajectory(v,u)
 
@@ -44,7 +46,7 @@ for v,u in zip(v_list,u_list):
     ymax = (v*v/g)-0.5*g*(v/g)*(v/g)
     print(ymax)
 
-    #Calculo el tiempo de vuelo para cada velocidad
+    #Calculo el tiempo de vuelo para cada velocidad inicial
     print("Tiempo de vuelo de cada velocidad",v)
     t_flight = 2*v/g
     print(t_flight)
